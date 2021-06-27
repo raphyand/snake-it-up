c091ab8c567513e248b4340431899444264da9ae
Raphael S. Andaya <raphyand@gmail.com>
2021-06-27 15:17:16 -0700

    Added heading to Utils.py

---
+++ utils.py
@@ -0,0 +1,12 @@
+"""Utilties module for enumerations of AI behavior and game states"""
+#_author__ = 'Raphael S. Andaya'
+# __email__ = 'raphyand@csu.fullerton.edu'
+#__maintainer__ = 'raphyand'
+
+from enum import Enum
+class GameState(Enum):
+    """Game State Enumerations"""
+    END_MENU = -1
+    MAIN_MENU = 0
+    PLAY_LEVEL = 1
+    LEADER_BOARD = 2
